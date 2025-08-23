25b6306efa216cd42f747414e46e3bfbe1c5045d
Samuel Pickard <sam@3degrees.co>
2025-08-23 15:57:37 +0100

    test(PLY-81): Add unit test for MusicLibraryHubScreen.getContent with explicit JUnit imports

--- app/src/test/java/com/foxy/player/music/MusicLibraryHubTest.kt
+++ app/src/test/java/com/foxy/player/music/MusicLibraryHubTest.kt
@@ -1,6 +1,8 @@
 package com.foxy.player.music
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class MusicLibraryHubTest {
