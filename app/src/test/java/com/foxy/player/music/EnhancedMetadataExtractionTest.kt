f3e8f474aced37ee1e6349618853a4d2c77012e2
Sam-3Degrees <sam@3degrees.co>
2025-09-21 22:09:22 +0100

    Fix wildcard import style violation

--- app/src/test/java/com/foxy/player/music/EnhancedMetadataExtractionTest.kt
+++ app/src/test/java/com/foxy/player/music/EnhancedMetadataExtractionTest.kt
@@ -6,7 +6,9 @@ import com.foxy.player.authentication.network.AuthenticatedApiClient
 import com.foxy.player.music.business.MusicMetadataExtractor
 import com.foxy.player.music.network.MusicDiscoveryService
 import kotlinx.coroutines.runBlocking
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class EnhancedMetadataExtractionTest {
